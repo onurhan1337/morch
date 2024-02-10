7d47c57b41215d76f54c66c1e92579afdf494ee4
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-02-11 02:00:34 +0300

    fix: refactor API endpoint for running model with error handling

--- src/pages/api/predictions/index.ts
+++ src/pages/api/predictions/index.ts
@@ -9,28 +9,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-  const output = await replicate.run(
-    "fofr/sdxl-emoji:dee76b5afde21b0f01ed7925f0665b7e879c50ee718c5f78a9d38e04d523cc5e",
-    {
-      input: {
-        width: 1024,
-        height: 1024,
-        prompt: prompt,
-        refine: "no_refiner",
-        scheduler: "K_EULER",
-        lora_scale: 0.6,
-        num_outputs: 1,
-        guidance_scale: 7.5,
-        apply_watermark: false,
-        high_noise_frac: 0.8,
-        negative_prompt: "",
-        prompt_strength: 0.8,
-        num_inference_steps: 50,
-      },
-    }
-  );
+    const output = await replicate.run(
+      "fofr/sdxl-emoji:dee76b5afde21b0f01ed7925f0665b7e879c50ee718c5f78a9d38e04d523cc5e",
+      {
+        input: {
+          width: 1024,
+          height: 1024,
+          prompt: prompt,
+          refine: "no_refiner",
+          scheduler: "K_EULER",
+          lora_scale: 0.6,
+          num_outputs: 1,
+          guidance_scale: 7.5,
+          apply_watermark: false,
+          high_noise_frac: 0.8,
+          negative_prompt: "",
+          prompt_strength: 0.8,
+          num_inference_steps: 50,
+        },
+      }
+    );
 
-  res.status(201).json(output);
+    res.status(201).json(output);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while running the model" });
+  }
 }
