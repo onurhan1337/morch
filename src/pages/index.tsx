f99d49867003cf6dd7823500bb3993bb0878a714
Onurhan <onurhandtr@gmail.com>
2024-02-13 22:50:19 +0300

    feat: add favicon and add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import { Inter } from "next/font/google";
 import Cta from "@/components/cta";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-fit flex-col items-center justify-between p-12 sm:p-24 ${inter.className}`}
-    >
-      <Cta />
-    </main>
+    <>
+      <Head>
+        <title>Morch - AI based emoji generator</title>
+        <meta
+          name="description"
+          content="Morch is an AI based emoji generator that helps you create custom emojis for your projects."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`flex min-h-fit flex-col items-center justify-between p-12 sm:p-24 ${inter.className}`}
+      >
+        <Cta />
+      </main>
+    </>
   );
 }
